3e34973a02aef1960b428e74f6a5874319c515d7
Arya Putatunda <arya6v9000@gmail.com>
2024-05-07 16:45:11 +0530

    final commit

---
+++ app/src/main/java/com/example/cinesync/Activity/IntroActivity.java
@@ -0,0 +1,31 @@
+package com.example.cinesync.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.cinesync.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        AppCompatButton getinBtn=findViewById(R.id.getInBtn);
+        getinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
